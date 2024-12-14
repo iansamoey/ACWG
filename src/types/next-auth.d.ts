13a7731e6505c1d12190aeeda65febc965ddd419
Ian Samoey1 <ian.samoey1@jtl.co.ke>
2024-12-14 20:59:22 +0300

    conflicts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,22 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+
 declare module "next-auth" {
-  interface User {
-    id: string
-    email: string
-    name: string
-    isAdmin?: boolean
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      isAdmin: boolean;
+    } & DefaultSession["user"];
   }
 
-  interface Session {
-    user: User
+  interface User extends DefaultUser {
+    isAdmin: boolean;
   }
 }
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    isAdmin: boolean;
+  }
+}
+
